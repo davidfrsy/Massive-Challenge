3fc8f4ddf4275f1dace56573d13919a7f6e594a7
davidfrsy <mdavidfrsy@gmail.com>
2024-06-24 21:52:33 +0700

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,10 +1,14 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "aquacare",
+  // host: "localhost",
+  // user: "root",
+  // password: "",
+  // database: "aquacare",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAM
 });
 
 db.connect((err) => {
